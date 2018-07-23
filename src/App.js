567bc8644121595123b1ecc04d03c70955395361
Viiralt <viiralt.h@gmail.com>
2018-07-23 21:25:37 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import BookDetail from './components/BookDetail';
 import Dashboard from './components/Dashboard';
+import ErrorPath from './components/ErrorPath';
+
 import { AppWrapper } from './elements';
 
 class App extends Component {
   render() {
     return (
       <AppWrapper>
-        <Dashboard />
+        <Router>
+          <Switch>
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/:book" component={BookDetail} />
+            <Route component={ErrorPath} />
+          </Switch>
+        </Router>
       </AppWrapper>
     );
   }
---
+++ src/components/ErrorPath.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { HeadingSmall } from '../elements';
+
+const ErrorPath = () => <HeadingSmall>Error: This path does not exist..</HeadingSmall>;
+
+export default ErrorPath;
