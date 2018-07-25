3826ca613731ca453085b8576e33b62dd0ce55a4
Viiralt <viiralt.h@gmail.com>
2018-07-25 13:29:58 +0300

    bug fixes

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,14 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { fetchBooksWatcher } from './fetchBooksSaga';
-<<<<<<< HEAD
-/* import { createBookWatcher } from './createBookSaga'; */
-
-export default function* rootSaga() {
-  yield all([fetchBooksWatcher()]);
-=======
 import { createBookWatcher } from './createBookSaga';
 
 export default function* rootSaga() {
   yield all([fetchBooksWatcher(), createBookWatcher()]);
->>>>>>> feat
 }
